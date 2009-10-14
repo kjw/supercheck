d350863b70779562684b84c8d6c0c754fe46a179
Karl Jonathan Ward <karl.j.ward@googlemail.com>
2009-10-14 13:41:15 +0100

    ParameterBunch.java: Fixed indentation.

--- src/tbc/supercheck/ParameterBunch.java
+++ src/tbc/supercheck/ParameterBunch.java
@@ -17,62 +17,62 @@ public class ParameterBunch {
 		FLOAT,
 		INT
 	}
-	
+
 	private class ParamEntry {
 		private Object value;
 		private ParamType type;
 	}
-	
+
 	private Hashtable<String, ParamEntry> parameters = new Hashtable<String, ParamEntry>();
-	
+
 	public void setFloat(String name, float val) {
 		ParamEntry pe = new ParamEntry();
 		pe.value = new Float(val);
 		pe.type = ParamType.FLOAT;
-    	parameters.put(name, pe);
-    }
-	
+		parameters.put(name, pe);
+	}
+
 	public void setInt(String name, int val) {
 		ParamEntry pe = new ParamEntry();
 		pe.value = new Integer(val);
 		pe.type = ParamType.INT;
-    	parameters.put(name, pe);
-    }
-	
+		parameters.put(name, pe);
+	}
+
 	public void setStr(String name, String val) {
 		ParamEntry pe = new ParamEntry();
 		pe.value = val;
 		pe.type = ParamType.STRING;
-    	parameters.put(name, pe);
-    }
-    
-    public float getFloat(String name) {
-    	ParamEntry pe = parameters.get(name);
-    	
-    	if (pe != null && pe.type == ParamType.FLOAT) {
-    		return (Float)pe.value;
-    	} else {
-    		return 0.0f;
-    	}
-    }
-    
-    public int getInt(String name) {
-    	ParamEntry pe = parameters.get(name);
-    	
-    	if (pe != null && pe.type == ParamType.INT) {
-    		return (Integer)pe.value;
-    	} else {
-    		return 0;
-    	}
-    }
-    
-    public String getStr(String name) {
-    	ParamEntry pe = parameters.get(name);
-    	
-    	if (pe != null && pe.type == ParamType.STRING) {
-    		return (String)pe.value;
-    	} else {
-    		return "";
-    	}
-    }
+		parameters.put(name, pe);
+	}
+
+	public float getFloat(String name) {
+		ParamEntry pe = parameters.get(name);
+
+		if (pe != null && pe.type == ParamType.FLOAT) {
+			return (Float)pe.value;
+		} else {
+			return 0.0f;
+		}
+	}
+
+	public int getInt(String name) {
+		ParamEntry pe = parameters.get(name);
+
+		if (pe != null && pe.type == ParamType.INT) {
+			return (Integer)pe.value;
+		} else {
+			return 0;
+		}
+	}
+
+	public String getStr(String name) {
+		ParamEntry pe = parameters.get(name);
+
+		if (pe != null && pe.type == ParamType.STRING) {
+			return (String)pe.value;
+		} else {
+			return "";
+		}
+	}
 }
